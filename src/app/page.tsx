90a39acc2688e1b2e6a2090d56f0e97c8d69c584
Edwilson <edwsilva@gmail.com>
2024-07-02 10:41:56 -0300

    Ajuste footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
-// import Container from "./components/Layout/Container/Container";
+import React from "react";
+import Slider from "react-slick";
 import styles from "./home.module.css";
 import { GoArrowRight, GoArrowDown } from "react-icons/go";
 import { MdBusinessCenter, MdFactCheck, MdAssignmentAdd } from "react-icons/md";
 
 export default function Home() {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    // <Container>
     <div className={styles.contentContainer}>
       <h3 className={styles.title}>O que é esse serviço?</h3>
       <div className={styles.mainContainer}>
@@ -49,6 +84,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-    // </Container>
   );
 }
